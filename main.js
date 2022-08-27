210a687e46a2889c74f416f7651b93f5d081c2c9
jorge diaz <jorge.diaz3887@gmail.com>
2022-08-27 00:12:09 -0300

    fix en funcion de clima

--- main.js
+++ main.js
@@ -39,22 +39,25 @@ let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
 
 //Petición a la Api del Clima
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?lat=-24.183084917115796&lon=-65.33127644531041&appid=555083fd438613a78b0609408b019c59"
-)
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json);
-    actualizarValoresClima(json);
-  })
-  .catch((err) => console.log(err));
+async function clima() {
+  let response = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=-24.183084917115796&lon=-65.33127644531041&appid=555083fd438613a78b0609408b019c59"
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      actualizarValoresClima(json);
+    })
+    .catch((err) => console.log(err));
+}
+clima();
 
 // Funcion para convertir de grados kelvin a grados celsius
 function convertirGradosCelsius(grados_Kelvin) {
   return parseFloat(grados_Kelvin) - 273.15;
 }
 //Actualizar valores del clima en la vista
-function actualizarValoresClima(clima) {  
+function actualizarValoresClima(clima) {
   let nombre_ciudad = clima.name;
   let temp = convertirGradosCelsius(clima.main.temp);
   let temp_max = convertirGradosCelsius(clima.main.temp_max);
