0ead2998247402e33008b6c20e1f6baf990622df
jorge diaz <jorge.diaz3887@gmail.com>
2022-08-26 21:29:52 -0300

    se agrego fecha del evento, se posiciono imagenes, se agrego sombra a las imagenes, cambio de colory tamaño de fuente

--- main.js
+++ main.js
@@ -1,69 +1,80 @@
-function onClick (event) {
-    event.preventDefault();
-    this.style.backgroundColor = "black";
-    console.log("click...");
-    console.log(event);
-  
-  
-    const mensaje = {
-      comercio: document.getElementById('comercio').value,
-      titular: document.getElementById('titular').value,
-      celular: document.getElementById('celular').value
-    }
-    console.log(mensaje);
-  
-  
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify(mensaje),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
+function onClick(event) {
+  event.preventDefault();
+  // this.style.backgroundColor = "black";
+  console.log("click...");
+  console.log(event);
+
+  const mensaje = {
+    comercio: document.getElementById("comercio").value,
+    titular: document.getElementById("titular").value,
+    celular: document.getElementById("celular").value,
+  };
+  console.log(mensaje);
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(mensaje),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      let notificacion =
+        "Gracias  " +
+        json.titular +
+        "  por registrate en la ExpoJuy 2022, tu numero de registro es: " +
+        json.id;
+      Swal.fire("Enviado", notificacion, "success");
+      cleanForm();
+      /* redirectUrl(); */
     })
-      .then((response) => response.json())
-      .then((json) => { 
-          console.log(json);
-          let notificacion = 'Gracias  '+json.titular+'  por registrate en la ExpoJuy 2022, tu numero de registro es: '+json.id;
-          Swal.fire(
-              'Enviado',
-              notificacion,
-              'success'
-          );
-          cleanForm();
-          /* redirectUrl(); */
-      })
-      .catch((err) => console.log(err));
-  
-  }
-  function cleanForm() {
-    let formulario = document.getElementById('formulario');    
-    formulario.reset();    
+    .catch((err) => console.log(err));
 }
-function redirectUrl(){
-    window.location.href = "https://google.com";    
+function cleanForm() {
+  let formulario = document.getElementById("formulario");
+  formulario.reset();
 }
 
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
 
-//Función del Clima
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=-24.183084917115796&lon=-65.33127644531041&appid=555083fd438613a78b0609408b019c59')
-.then((response) => response.json())
-.then((json) => { 
+//Petición a la Api del Clima
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?lat=-24.183084917115796&lon=-65.33127644531041&appid=555083fd438613a78b0609408b019c59"
+)
+  .then((response) => response.json())
+  .then((json) => {
     console.log(json);
-    //Actualizar valores del clima
-    let temp = parseFloat(json.main.temp) - 273.15; 
-    let temp_max = parseFloat(json.main.temp_max) - 273.15; 
-    let temp_min = parseFloat(json.main.temp_min) - 273.15; 
-    let visibilidad = parseInt(json.visibility)/1000;
-    document.getElementById('ciudad').textContent = json.name; 
-    document.getElementById('temp').textContent = temp.toFixed(2)+' °C'; 
-    document.getElementById('temp_max').textContent = 'MAX: '+temp_max.toFixed(2)+' °C'; 
-    document.getElementById('temp_min').textContent = 'MIN: '+temp_min.toFixed(2)+' °C'; 
-    document.getElementById('humedad').textContent = 'HUMEDAD: '+json.main.humidity+ '%';
-    document.getElementById('presion').textContent = 'PRESIÓN: '+json.main.pressure+' hPa';
-    document.getElementById('visibilidad').textContent = 'VISIBILIDAD: '+visibilidad+' Km';
-    //Agregar icono del clima
-    let icon = json.weather[0].icon;
-    document.getElementById('clima-icon').src = "http://openweathermap.org/img/wn/"+icon+"@2x.png"; 
+    actualizarValoresClima(json);
+  })
+  .catch((err) => console.log(err));
 
-})
-.catch((err) => console.log(err));
+// Funcion para convertir de grados kelvin a grados celsius
+function convertirGradosCelsius(grados_Kelvin) {
+  return parseFloat(grados_Kelvin) - 273.15;
+}
+//Actualizar valores del clima en la vista
+function actualizarValoresClima(clima) {  
+  let nombre_ciudad = clima.name;
+  let temp = convertirGradosCelsius(clima.main.temp);
+  let temp_max = convertirGradosCelsius(clima.main.temp_max);
+  let temp_min = convertirGradosCelsius(clima.main.temp_min);
+  let humedad = clima.main.humidity;
+  let presion = clima.main.pressure;
+  let visibilidad = parseInt(clima.visibility) / 1000;
+  document.getElementById("ciudad").textContent = nombre_ciudad;
+  document.getElementById("temp").textContent = temp.toFixed(2) + " °C";
+  document.getElementById("temp_max").textContent =
+    "MAX: " + temp_max.toFixed(2) + " °C";
+  document.getElementById("temp_min").textContent =
+    "MIN: " + temp_min.toFixed(2) + " °C";
+  document.getElementById("humedad").textContent = "HUMEDAD: " + humedad + "%";
+  document.getElementById("presion").textContent =
+    "PRESIÓN: " + presion + " hPa";
+  document.getElementById("visibilidad").textContent =
+    "VISIBILIDAD: " + visibilidad + " Km";
+  //Agregar icono del clima
+  let icon = clima.weather[0].icon;
+  document.getElementById("clima-icon").src =
+    "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+}
